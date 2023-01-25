91a16aa9653f33fa7929541b52b73b2c5fa9b56c
studiofox <119687409+studiofox@users.noreply.github.com>
2023-01-25 17:30:38 +0530

    script.js

--- script.js
+++ script.js
@@ -70,19 +70,25 @@
 	
 	function onProgress(xhr) { console.log( (xhr.loaded / xhr.total * 100) + '% loaded' ); }
 	function onError(xhr) { console.log( 'An error happened' ); }
+				
 
 
-	const assetLoader = new THREE.GLTFLoader();
+
+
+
+
+
+	const loader = new THREE.GLTFLoader();
 	
-
-assetLoader.load("box.glb", function (gltf) {
-  const model = gltf.scene;
+let model;
+loader.load("box.glb", function (gltf) {
+   model = gltf.scene;
   markerRoot1.add(model);
-//   model.position.set(-12, -50, -100);
-  model.scale.set(0.7, 0.7, 0.7);
+  model.position.set(0, 0, 0.5);
+  model.scale.set(0.8, 0.8, 0.8);
 
   setTimeout(() => {
-	model.position.set(-1, 0, 0);
+	model.position.set(-1.2, 0, 0.5);
   }, 6500);
   setTimeout(() => {
 	markerRoot1.remove(model);
@@ -95,40 +101,23 @@ assetLoader.load("box.glb", function (gltf) {
 //   }, 31000);
 
 
+
+
+
   model.traverse(function(node) {
     if(node.isMesh)
     {node.castShadow = true;}
   });
+
+// return model;
+
 });
 
-// let model;
-// assetLoader.load("box.glb", function (gltf) {
-//   const model = gltf.scene;
-//   markerRoot1.add(model);
-// //   model.position.set(-12, -50, -100);
-//   model.scale.set(0.7, 0.7, 0.7);
-
-// //   const tween1 = new TWEEN.Tween({x: 0, y:0, z:0})
-// //   .to({x: 5, y: 0, z: 0}, 2000)
-// //   .onUpdate((coords) => {
-// // 	model.position.x = coords.x;
-// // 	model.position.y = coords.y;
-// // 	model.position.z = coords.z;
-// //   })
-// // //   .easiing(TWEEN.Easing.Exponential.InOut)
-// //   .repeat(Infinity)
-// //   tween1.start();
+	
 
 
-  
-  
-//   model.traverse(function(node) {
-//     if(node.isMesh)
-//     {node.castShadow = true;}
-//   });
-// });
 
-const plane1geo = new THREE.PlaneGeometry(1.5, 1.5, 1.5);
+const plane1geo = new THREE.PlaneGeometry(2, 2, 2);
 const plane1texture = new THREE.TextureLoader().load("Picture1.png")
 const plane1Mat = new THREE.MeshBasicMaterial({
 	map : plane1texture,
@@ -136,8 +125,40 @@ const plane1Mat = new THREE.MeshBasicMaterial({
 });
 const plane1 = new THREE.Mesh(plane1geo, plane1Mat);
 // markerRoot1.add(plane1);
-plane1.position.set(1, 0, -0.5);
+plane1.position.set(10, 0, -0.5);
 // plane1.scale.set(0, 0, 0);
+
+// POSITION
+const positionKF = new THREE.VectorKeyframeTrack( '.position', [ 0, 1, 2 ], [ 10, 0, -0.5, 1.2, 0, -0.5, 1.2, 0, -0.5 ] );
+
+const positionKF1 = new THREE.VectorKeyframeTrack( '.position', [ 0, 1, 2 ], [  1.2, 0, -0.5, 10, 0, -0.5, 10, 0, -0.5] );
+
+// // SCALE
+// const scaleKF = new THREE.VectorKeyframeTrack( '.scale', [ 0, 1, 2 ], [ 0, 0, 0,1, 1, 1, 1, 1, 1, ] );
+
+
+// // OPACITY
+// const opacityKF = new THREE.NumberKeyframeTrack( '.material.opacity', [ 1, 1, 2 ], [ 1, 1, 2 ] );
+
+// create an animation sequence with the tracks
+// If a negative time value is passed, the duration will be calculated from the times of the passed tracks array
+const clip = new THREE.AnimationClip( 'Action', 50, [ positionKF ] );
+const clip1 = new THREE.AnimationClip( 'Action', 50, [ positionKF1 ] );
+
+ 
+// setup the THREE.AnimationMixer
+const mixer = new THREE.AnimationMixer( plane1);
+const mixer1 = new THREE.AnimationMixer( plane1);
+			
+
+// create a ClipAction and set it to play
+const clipAction = mixer.clipAction( clip );
+const clipAction1 = mixer1.clipAction( clip1 );
+
+
+
+
+
 
 const plane4geo = new THREE.PlaneGeometry(2, 1, 1.5);
 const plane4texture = new THREE.TextureLoader().load("Picture5.png")
@@ -146,9 +167,29 @@ const plane4Mat = new THREE.MeshBasicMaterial({
 	side : THREE.DoubleSide
 });
 const plane4 = new THREE.Mesh(plane4geo, plane4Mat);
-// markerRoot4.add(plane4);
-plane4.position.set(2, 2, -0.5);
-// plane1.scale.set(0, 0, 0);
+// markerRoot1.add(plane4);
+plane4.position.set(1.2, 0, 10);
+plane4.rotation.set(-Math.PI/2,0,0)
+
+
+// POSITION
+const positionKF2 = new THREE.VectorKeyframeTrack( '.position', [ 0, 1, 2 ], [ 1.2, 0, 10, 1.2, 0, 1, 1.2, 0, 1 ] );
+
+const positionKF3 = new THREE.VectorKeyframeTrack( '.position', [ 0, 1, 2 ], [ 1.2, 0, 1, 1.2, 0, 10, 1.2, 0, 10 ] );
+
+const clip2 = new THREE.AnimationClip( 'Action', 50, [ positionKF2 ] );
+const clip3 = new THREE.AnimationClip( 'Action', 50, [ positionKF3 ] );
+
+ // setup the THREE.AnimationMixer
+const mixer2 = new THREE.AnimationMixer( plane4);
+const mixer3 = new THREE.AnimationMixer( plane4);
+			
+// create a ClipAction and set it to play
+const clipAction2 = mixer2.clipAction( clip2 );
+const clipAction3 = mixer3.clipAction( clip3 );
+
+
+
 
 
 const plane2geo = new THREE.PlaneGeometry(2.5, 1.5, 1.5);
@@ -169,7 +210,8 @@ const plane5Mat = new THREE.MeshBasicMaterial({
 });
 const plane5 = new THREE.Mesh(plane5geo, plane5Mat);
 // markerRoot1.add(plane5);
-plane5.position.set(0, 2, -0.5);
+plane5.position.set(0, 0, 1.2);
+plane5.rotation.set(-Math.PI/2,0,0)
 
 
 
@@ -194,39 +236,60 @@ function update()
 
 function render()
 {
+    if ( mixer ) {
+
+	const delta = clock.getDelta();
+	mixer.update( delta );
+	mixer1.update( delta );
+	mixer2.update( delta );
+	mixer3.update( delta );
+
+	}
+
 	renderer.render( scene, camera );
 }
 
-setTimeout(() => {
+// setTimeout(() => {
+// 	markerRoot1.add(plane1);
+//   }, 8000);
+  setTimeout(() => {
 	markerRoot1.add(plane1);
-  }, 8000);
+	clipAction.play();
+  }, 6500);
   setTimeout(() => {
 	markerRoot1.add(plane4);
+	clipAction2.play();
   }, 8500);
   setTimeout(() => {
-	markerRoot1.remove(plane4);
+	clipAction3.play();
   }, 15000);
   setTimeout(() => {
-	markerRoot1.remove(plane1);
+	markerRoot1.remove(plane4);
   }, 16000);
   setTimeout(() => {
-	markerRoot1.add(plane2);
+	clipAction1.play();
   }, 17000);
   setTimeout(() => {
-	markerRoot1.add(plane5);
+	markerRoot1.remove(plane1);
   }, 18000);
-  setTimeout(() => {
-	markerRoot1.remove(plane5);
-  }, 43500);
-  setTimeout(() => {
-	markerRoot1.remove(plane2);
-  }, 45000);
-  setTimeout(() => {
-	markerRoot1.add(plane3);
-  }, 46000);
-  setTimeout(() => {
-	markerRoot1.remove(plane3);
-  }, 56000);
+//   setTimeout(() => {
+// 	markerRoot1.add(plane2);
+//   }, 17000);
+//   setTimeout(() => {
+// 	markerRoot1.add(plane5);
+//   }, 18000);
+//   setTimeout(() => {
+// 	markerRoot1.remove(plane5);
+//   }, 43500);
+//   setTimeout(() => {
+// 	markerRoot1.remove(plane2);
+//   }, 45000);
+//   setTimeout(() => {
+// 	markerRoot1.add(plane3);
+//   }, 46000);
+//   setTimeout(() => {
+// 	markerRoot1.remove(plane3);
+//   }, 56000);
 //   setTimeout(() => {
 // 	markerRoot1.add(plane1);
 //   }, 27000);
@@ -268,18 +331,9 @@ function animate()
 
 animate();
 
-// function settime(){
-// 	var audio= document.getElementById("myaudio");
-// 	audio.currentTime=0;
-// 	audio.duration = 5;
-//     audio.ended = 15;
-// 	audio.play();
-			
-// 	}
-
 	setTimeout(function(){
 		document.getElementById("myaudio1").play();
-		}, 9500)
+		}, 10500)
 
 		setTimeout(function(){
 			document.getElementById("myaudio2").play();
